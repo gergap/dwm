21132d55c4bf85f47ee2614e3878e0e696628d4e
Gerhard Gappmeier <gerhard.gappmeier@ascolab.com>
2020-09-21 09:17:06 +0200

    add support for volume keys

--- config.h
+++ config.h
@@ -68,6 +68,9 @@ static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont,
 static const char *termcmd[]  = { "terminator", NULL };
 static const char *webcmd[]   = { "chromium", NULL };
 static const char *webaltcmd[] = { "firefox-esr", NULL };
+static const char *upvol[]   = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%",     NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%",     NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute",   "0", "toggle",  NULL };
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -108,6 +111,9 @@ static Key keys[] = {
 	TAGKEYS(                        XK_8,                      7)
 	TAGKEYS(                        XK_9,                      8)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
+	{ 0,                            XF86XK_AudioLowerVolume,   spawn, { .v = downvol } },
+	{ 0,                            XF86XK_AudioMute,          spawn, { .v = mutevol } },
+	{ 0,                            XF86XK_AudioRaiseVolume,   spawn, {.v = upvol } },
 };
 
 /* button definitions */
