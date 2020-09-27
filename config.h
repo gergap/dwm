b7bb324501f2ab5e139b3ef0c47c9fbc278e2b26
Gerhard Gappmeier <gerhard.gappmeier@ascolab.com>
2020-09-27 14:29:46 +0200

    fix xosview detection

--- config.h
+++ config.h
@@ -40,7 +40,7 @@ static const Rule rules[] = {
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       1 << 7,       1,           -1 },
-	{ "xosview",  NULL,       NULL,       0,            1,           -1 },
+	{ "XOsview",  NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Chromium", NULL,       NULL,       1 << 8,       0,           -1 },
 };
