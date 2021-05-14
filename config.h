f0f72257c755d80c66ba9fa21631c3b8d88572ca
Gerhard Gappmeier <gerhard.gappmeier@ascolab.com>
2021-05-14 09:57:58 +0200

    Make XClock and XEyes floating windows

--- config.h
+++ config.h
@@ -56,6 +56,8 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       1 << 7,       1,           -1 },
 	{ "XOsview",  NULL,       NULL,       0,            1,           -1 },
+	{ "XClock",   NULL,       NULL,       0,            1,           -1 },
+	{ "XEyes",    NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
 	{ "Chromium", NULL,       NULL,       1 << 8,       0,           -1 },
 };
